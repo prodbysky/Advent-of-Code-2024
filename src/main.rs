12fc2dd6f544ecf2818760bbc8be7d8be801131a
prodbysky <kyrbynyt@gmail.com>
2024-12-02 07:16:42 +0200

    Day 2 part 1 complete
    d2p2 when i come back from school

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
 use std::fs::read_to_string;
 
+fn main() {
+    println!("Day 1, part 1: {}", day1_part1());
+    println!("Day 1, part 2: {}", day1_part2());
+
+    println!("Day 2, part 1: {}", day2_part1());
+    println!("Day 2, part 2: {}", day2_part2());
+}
+
+fn day2_part1() -> i32 {
+    let input_name = "inputs/d2_test.txt";
+    let input = read_to_string(input_name).unwrap();
+    let mut count = 0;
+
+    let valid = |measurements: &[i32]| -> bool {
+        let mut i = 1;
+        let increasing = measurements[0] < measurements[1];
+        while i < measurements.len() {
+            let curr = measurements[i];
+            let prev = measurements[i - 1];
+            let diff = (curr - prev).abs();
+            if !(1..=3).contains(&diff) {
+                return false;
+            }
+            if curr < prev && increasing {
+                return false;
+            }
+            if curr > prev && !increasing {
+                return false;
+            }
+            i += 1;
+        }
+        true
+    };
+
+    return input
+        .lines()
+        .map(|line| line.split_whitespace())
+        .map(|split| split.map(|x| x.parse::<i32>().unwrap()))
+        .map(|parsed| valid(&parsed.collect::<Vec<_>>()) as i32)
+        .sum();
+}
+fn day2_part2() -> i32 {
+    0
+}
+
 fn day1_part1() -> i32 {
-    let input_name = "inputs/d1p1_test.txt";
+    let input_name = "inputs/d1_test.txt";
     let input = read_to_string(input_name).unwrap();
     let mut left = vec![];
     let mut right = vec![];
@@ -18,7 +63,7 @@ fn day1_part1() -> i32 {
 }
 
 fn day1_part2() -> i32 {
-    let input_name = "inputs/d1p1_test.txt";
+    let input_name = "inputs/d1_test.txt";
     let input = read_to_string(input_name).unwrap();
     let mut left = vec![];
     let mut right = vec![];
@@ -38,9 +83,4 @@ fn day1_part2() -> i32 {
         score += count * x;
     }
     score
-}
-
-fn main() {
-    println!("Day 1, part 1: {}", day1_part1());
-    println!("Day 1, part 2: {}", day1_part2());
 }
